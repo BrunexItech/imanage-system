1d51880d752a11b79c192c646558ff4d13221421
BrunexItech <brunosharif89@gmail.com>
2026-02-10 21:21:44 +0300

    Updating websockets

--- backend/imanage/asgi.py
+++ backend/imanage/asgi.py
@@ -1,10 +1,17 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# SET DJANGO_SETTINGS_MODULE FIRST
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imanage.settings')
+
+# Setup Django BEFORE importing apps
+django.setup()
+
+# Now import WebSocket routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from sales.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imanage.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
